035ee7fb47ed9ace1ec091bec297802abb1aa693
blogabe <6888675+blogabe@users.noreply.github.com>
2020-06-26 16:12:38 -0700

    adding totalmarker and cleaning up images dir

--- config/scripts/xp-setup.sh
+++ config/scripts/xp-setup.sh
@@ -1,45 +1,96 @@
-echo "Setting directory structure and linking files"
-cd ${XPLANET_HOME}
+echo "Setting home directory structure and linking cloud and marker files"
+cd $XPLANET_HOME
 mkdir -p {arcs,images,logs,markers,satellites}
-${XPLANET_CONFIG}/scripts/xplanet.sh earth
-cd ${XPLANET_HOME}/images
-ln -s ${XPLANET_CONFIG}/images/clouds-4096.jpg clouds-4096.jpg
-ln -s ${XPLANET_CONFIG}/images/earth_lights_4800.tiff lights.tiff
-ln -s ${XPLANET_CONFIG}/images/gebco_08_rev_elev_21600x10800.png bump.png
-ln -s ${XPLANET_CONFIG}/images/specular.png specular.png
-cd ${XPLANET_HOME}/markers
-ln -s $(brew --prefix)/share/xplanet/markers/earth earth
+$XPLANET_CONFIG/scripts/xplanet.sh earth
+cd $XPLANET_HOME/images
+ln -s $XPLANET_CONFIG/images/clouds/clouds-4096.jpg clouds-4096.jpg
+ln -s $XPLANET_CONFIG/images/lights/earth_lights_4800.tiff lights.tiff
+ln -s $XPLANET_CONFIG/images/bump/gebco_08_rev_elev_21600x10800.png bump.png
+ln -s $XPLANET_CONFIG/images/specular/specular.png specular.png
+cd $XPLANET_HOME/markers
+ln -s $XPLANET_CONFIG/earth earth
 
-echo ""
-echo -e "
-  The final part of the setup is to make Xplanet startup at login.
+echo "Configuring Xplanet"
+echo -e "################################################################################
+  Xplanet, on its own, is installed and configured. The rest of the setup,
+  and only with your approval, will update the marker files and the cloud image
+  in addition to automating the script execution to keep everything up to date.
   
-  If you do not want to do this, Control-C now.
-  
-  If you want to automate Xplanet's startup and updates, edit the
-  working directory in the 3 files located at ${XPLANET_CONFIG}/scripts/plist
-  by replacing '[username]' with '$USER'
-  
-"
-echo 'Press any key to continue or Control-C to exit...'; read -k1 -s
+  Step 1) Installs a necessary PERL module for Totalmarker to work and updates
+          the marker files.
 
-echo "Copying plist files to automate startup and updates"
-mkdir -p /Users/$USER/Library/LaunchAgents
-cd /Users/$USER/Library/LaunchAgents
-cp ${XPLANET_CONFIG}/scripts/plist/local.xplanet.* .
+          Additionally, you can configure Totalmarker, e.g., QuakeMinimumSize,
+          by editing $XPLANET_CONFIG/totalmarker.ini
 
-echo "Starting Xplanet..."
-for plist_script (start earth-map clouds); do
-    if [ -f local.xplanet.$plist_script.plist ]; then
+  Step 2) Keeping the cloud image up to date requires a paid subscription to
+          Xeric Designs: https://www.xericdesign.com/xplanet.php
+
+          Subscribers need to update 'CloudUsername' and 'CloudPassword' in
+          $XPLANET_CONFIG/totalmarker.ini
+
+  Step 3) Automates Xplanet so that it loads on login and keeps the marker
+          files up to date.
+
+  You will be prompted to continue at each step."
+echo "################################################################################"
+echo 'Step 1) Press any key to continue or Control-C to exit...'; read -k1 -s
+
+echo "Prepare marker files with Totalmarker"
+cd $XPLANET_HOME
+TMPL_LOC=$XPLANET_CONFIG/scripts/Totalmarker2.6.1.pl
+ln -s $TMPL_LOC $TM
+# Important we get this right now
+if [ ! -L $TM ]; then
+    echo "ERROR! Xplanet environment setup failed"
+    echo "  Link to Totalmarker script doesn't point to anything... check and rerun"
+    exit 1
+fi
+echo "Installing PERL module"
+export PERL_MM_USE_DEFAULT=1
+cpan Mozilla::CA > /dev/null
+if [ ! -L $XPLANET_CONFIG/totalmarker.ini ]; then
+    echo "Running Totalmarker for the first (and second time)"
+    /usr/bin/perl $TM > /dev/null
+    /usr/bin/perl $TM > /dev/null
+    sed -i '' "s/clouds_2048.jpg/clouds-4096.jpg/" $XPLANET_CONFIG/totalmarker.ini
+    sed -i '' "s/Username/CloudUsername/" $XPLANET_CONFIG/totalmarker.ini
+    sed -i '' "s/Password/CloudPassword/" $XPLANET_CONFIG/totalmarker.ini
+    open -e $XPLANET_CONFIG/totalmarker.ini
+    echo "********************************************************************************"
+    echo "  Now is a good time to review and edit Totalmarker's initialization file at\n$XPLANET_CONFIG/totalmarker.ini"
+    echo "********************************************************************************"
+fi
+echo "Updating markers"
+/usr/bin/perl $TM -Volcano
+/usr/bin/perl $TM -Storm
+/usr/bin/perl $TM -Quake
+PLISTS=('start' 'earth-map' 'quake' 'storm' 'volcano')
+echo "################################################################################"
+read -k1 "YESNO?Step 2) Are you subscribing to Xeric Designs for the cloud updates? "
+if [[ "$YESNO" =~ ^[Yy]$ ]]; then
+    echo "\nUpdating cloud image"
+    unlink $XPLANET_HOME/images/clouds-4096.jpg
+    /usr/bin/perl $TM -Clouds
+    PLISTS+=('clouds')
+fi
+
+echo "\nAutomating script execution so that Xplanet loads and updates when you log in"
+echo "################################################################################"
+read -k1 "YESNO?Step 3) Do you want to automate Xplanet startup w/ Totalmarker updates? "
+if [[ "$YESNO" =~ ^[Yy]$ ]]; then
+    echo "\nCopying plist files to automate startup and updates"
+    sed -i '' "s/<USERNAME>/$USER/" $XPLANET_CONFIG/scripts/plist/*.plist
+    mkdir -p /Users/$USER/Library/LaunchAgents
+    cd /Users/$USER/Library/LaunchAgents
+
+    echo "Starting Xplanet..."
+    for plist_script in $PLISTS; do
+        cp $XPLANET_CONFIG/scripts/plist/local.xplanet.$plist_script.plist .
         launchctl load -w local.xplanet.$plist_script.plist
         launchctl start local.xplanet.$plist_script
-    else
-        echo "WARNING local.xplanet.$plist_script.plist doesn't exist in ~/Library/LaunchAgents!"
-        echo "Fix and start manually with:"
-        echo "  launchctl load -w local.xplanet.$plist_script.plist"
-        echo "  launchctl start local.xplanet.$plist_script"
-    fi
-done
-echo "Expect another Apple security approval if this is running for the first time (wait 20 seconds)"
-echo "Log out and back in after you approve"
-cd ${XPLANET_HOME}
+    done
+    echo "********************************************************************************"
+    echo "  You may receive another Apple security approval\n  if this is running for the first time (wait 10 seconds)"
+    echo "********************************************************************************"
+fi
+cd $XPLANET_HOME
